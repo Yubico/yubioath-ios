735e309565e362008f9dcd9c7e8a3d61cffca73f
Irina Rakhmanova <irina.rakhmanova@yubico.com>
2019-11-01 16:22:33 -0700

    # This is a combination of 4 commits. # This is the 1st commit message:
    Set width for text fields in 'Set Password' and 'New Account' scenes. + UITextFieldDelegate 'should return' method added.
    
    # This is the commit message #2:
    
    Comments Fixed.
    'Set Password' : saving on 'done' return key added.
    'Add New Account': handling QR scenario added.
    
    # This is the commit message #3:
    
    String extension moved to separate file.
    
    # This is the commit message #4:
    
    Fixed the way to invoke savePassword function after done key tapped. + Access modifier for constant changed in String extension since it was moved to the separate file.

---
+++ Authenticator/UI/Utils/StringExtension.swift
@@ -0,0 +1,13 @@
+//
+//  StringExtension.swift
+//  Authenticator
+//
+//  Created by Irina Rakhmanova on 11/4/19.
+//  Copyright © 2019 Irina Makhalova. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    internal static let unwindToMainViewController = "unwindToMain"
+}
