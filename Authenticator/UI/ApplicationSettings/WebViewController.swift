e016ebff03b2b933d9c589a57238141968645ec9
IrinaRakh <56280531+IrinaRakh@users.noreply.github.com>
2019-11-26 16:35:22 -0800

    Warnings fixed. NFC image rounded corners removed. ActivityIndicator added to WebView. App Tutorial changed to Tutorial. (#24)
    * warnings fixed + roundedCorners removed for NFC image.
    
    * activityIndicator added to WebView.
    
    * 'App Tutorial' changed to 'Tutorial'.
    
    * Comments to WebVIewViewController added.

--- Authenticator/UI/ApplicationSettings/WebViewController.swift
+++ Authenticator/UI/ApplicationSettings/WebViewController.swift
@@ -9,9 +9,13 @@
 import UIKit
 import WebKit
 
-class WebViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
+class WebViewController: UIViewController, WKUIDelegate {
 
     @IBOutlet var webView: WKWebView!
+    
+    // Using property insead of IBOutlet, because outlet is nil for some reason after inithialization and
+    // compiler throughs 'Fatal error: unecxpectedly found nil...' error message.
+    private var activityIndicator: UIActivityIndicatorView!
     
     var url: URL?
     
@@ -19,7 +23,19 @@ class WebViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
         let webConfig = WKWebViewConfiguration()
         webView = WKWebView(frame: .zero, configuration: webConfig)
         webView.uiDelegate = self
+        // Assigning webView to view since phone reboots when using view.addSubview(webView).
         view = webView
+    }
+    
+    // Setting up the activity indicator in ViewWillAppear before the view hierarchy is loaded into memory.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        activityIndicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 80, height: 80))
+        activityIndicator.center = webView.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.style = .gray
+        webView.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
     }
     
     override func viewDidLoad() {
@@ -30,5 +46,17 @@ class WebViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
         
         webView.navigationDelegate = self
         webView.allowsBackForwardNavigationGestures = true
+    }
+}
+
+// MARK: - WebKitNavigationDelegate
+
+extension WebViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        activityIndicator.stopAnimating()
+    }
+
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        activityIndicator.stopAnimating()
     }
 }
