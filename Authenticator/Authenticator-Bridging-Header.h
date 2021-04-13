4a013695fc1e8bda7517f5782aef067fc93ddf35
Jens Utbult <jens.utbult@yubico.com>
2021-04-13 10:28:35 +0200

    Added YubiKit as Swift Package.

--- Authenticator/Authenticator-Bridging-Header.h
+++ Authenticator/Authenticator-Bridging-Header.h
@@ -1,7 +1,7 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
+#ifndef Authenticator_Bridging_Header_h
+#define Authenticator_Bridging_Header_h
 
-#import <YubiKit/YubiKit.h>
+#import <YubiKit.h>
 #import <CommonCrypto/CommonCrypto.h>
 
+#endif
