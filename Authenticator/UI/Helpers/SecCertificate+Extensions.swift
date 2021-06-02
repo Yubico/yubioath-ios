9a64b51ce31c9d40606eb5ac1816d057c65fde9e
Jens Utbult <jens.utbult@yubico.com>
2021-06-02 16:00:57 +0200

    Verify that the requested certificate is the one loaded on the YubiKey.

--- Authenticator/UI/Helpers/SecCertificate+Extensions.swift
+++ Authenticator/UI/Helpers/SecCertificate+Extensions.swift
@@ -17,21 +17,8 @@ extension SecCertificate {
         return name as String?
     }
     
-    var publicKey: SecKey? {
-        let publicKey = SecCertificateCopyKey(self)
-        return publicKey
-    }
-    
     func tokenObjectId() -> String {
         let data = SecCertificateCopyData(self) as Data
         return data.sha256Hash().map { String(format: "%02X", $0) }.joined()
-        
-        /*
-        var error:Unmanaged<CFError>?
-        guard let key = self.publicKey, let cfData = SecKeyCopyExternalRepresentation(key, &error) else { return nil }
-        let data = cfData as Data
-        let result = data.sha256Hash().map { String(format: "%02X", $0) }.joined()
-        return result
- */
     }
 }
