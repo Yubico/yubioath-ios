1eb16febca393b620c2d964fb17e31be1a453b10
Jens Utbult <jens.utbult@yubico.com>
2021-06-15 21:48:26 +0200

    Disable smart card configuration for iOS older than 14.5.

--- Authenticator/UI/YubiKeyConfiguration/ConfigurationController.swift
+++ Authenticator/UI/YubiKeyConfiguration/ConfigurationController.swift
@@ -11,6 +11,33 @@ import UIKit
 
 class ConfigurationController: UITableViewController {
     
+    @IBOutlet var smartCardEnabledLabel: UILabel!
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if #available(iOS 14.5, *) { } else {
+            switch (indexPath.section, indexPath.row) {
+            case (3, 0):
+                let alert = UIAlertController(title: "Smart card extension is only available on iOS 14.5 and forward.", message: nil, completion: {})
+                self.present(alert, animated: true, completion: nil)
+            default: break
+            }
+        }
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if #available(iOS 14.5, *) {
+            return true
+        } else {
+            return identifier != "showSmartCardCertificates"
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if #available(iOS 14.5, *) {
+            smartCardEnabledLabel.isHidden = TokenCertificateStorage().listTokenCertificates().count == 0
+        }
+    }
+    
     deinit {
         print("Deinit ConfigurationController")
     }
