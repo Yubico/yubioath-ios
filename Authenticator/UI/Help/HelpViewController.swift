33b20b2439e861c5bc68c54b3c406d1348e47fb7
Jens Utbult <jens.utbult@yubico.com>
2021-06-18 12:21:28 +0200

    Nicer looking help page.

--- Authenticator/UI/Help/HelpViewController.swift
+++ Authenticator/UI/Help/HelpViewController.swift
@@ -17,28 +17,13 @@ class HelpViewController: UITableViewController {
 
     // MARK: - Table view data source
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = super.tableView(tableView, cellForRowAt: indexPath)
-        switch (indexPath.section, indexPath.row) {
-        case (2, 1):
-            cell.textLabel?.text = "Yubico Authenticator \(UIApplication.appVersion) (build \(UIApplication.appBuildNumber))"
-        default:
-            break
-        }
-        return cell
+    @IBOutlet weak var versionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        versionLabel.text = "\(UIApplication.appVersion) (build \(UIApplication.appBuildNumber))"
     }
     
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return false
-    }
-    
-    /*
-     Reset feature was removed for users due to it's complexity.
-     To get to the default state user can manually delete credentials and remove password under Settings.
-     To restore this feature, use git history and add a cell to SettingsViewController in the main storyboard.
-    */
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let storyBoard = UIStoryboard(name: "Main", bundle: nil)
         let webViewController = storyBoard.instantiateViewController(withIdentifier: "WebViewController") as! WebViewController
@@ -59,11 +44,6 @@ class HelpViewController: UITableViewController {
             title = title.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "[iOSAuthenticator]"
             webViewController.url = URL(string: "https://support.yubico.com/support/tickets/new?setField-helpdesk_ticket_subject=\(title)")
             self.navigationController?.pushViewController(webViewController, animated: true)
-            
-        case (2, 0):
-            DispatchQueue.main.async { [weak self] in
-                self?.performSegue(withIdentifier: "ShowWhatsNew", sender: self)
-            }
         default:
             break
         }
