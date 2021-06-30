88bd965b6733bb167b4602a269449a97ad1ba326
Jens Utbult <jens.utbult@yubico.com>
2021-06-30 09:20:53 +0200

    Added context menu on credential table cells to handle generate code and copy.

--- Authenticator/UI/Authentication/CredentialTableViewCell.swift
+++ Authenticator/UI/Authentication/CredentialTableViewCell.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import Combine
 
 class CredentialTableViewCell: UITableViewCell {
+    
+    var viewModel: OATHViewModel!
 
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var code: UILabel!
@@ -17,6 +20,11 @@ class CredentialTableViewCell: UITableViewCell {
     @IBOutlet weak var actionIcon: UIImageView!
     @IBOutlet weak var credentialIcon: UILabel!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    @IBOutlet weak var generateCodeButton: UIButton!
+    @IBOutlet weak var copyCodeButton: UIButton!
+    @IBOutlet weak var closeButton: UIButton!
     
     @IBOutlet weak var favoriteIcon: UIImageView!
     
@@ -27,20 +35,79 @@ class CredentialTableViewCell: UITableViewCell {
 
     private var credentialIconColor: UIColor = .primaryText
     
+    private var bag = [Cancellable]()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        actionIcon.isHidden = true
-        favouriteIcon.isHidden = true
-        progress.isHidden = true
-        activityIndicator.isHidden = true
+//        copyCodeButton.adjustsImageWhenDisabled = true
+//        copyCodeButton.tintAdjustmentMode = .dimmed
+        prepareForReuse()
         activityIndicator.startAnimating()
+        bag.append(closeButton.addHandler(for: .touchUpInside) {
+            self.setSelected(false, animated: true)
+        })
+        bag.append(copyCodeButton.addHandler(for: .touchUpInside) {
+            if let credential = self.credential {
+                self.viewModel.copyToClipboard(credential: credential)
+            }
+            self.setSelected(false, animated: true)
+        })
+        bag.append(generateCodeButton.addHandler(for: .touchUpInside) {
+            self.setSelected(false, animated: true)
+            if let credential = self.credential {
+                // Slight delay for calculating HOTP so the user can see the code updating
+                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + (credential.type == .HOTP ? 0.4 : 0)) {
+                    self.viewModel.calculate(credential: credential)
+                }
+            }
+        })
     }
     
     override func prepareForReuse() {
+        blurView.effect = nil
         actionIcon.isHidden = true
         favouriteIcon.isHidden = true
         progress.isHidden = true
         activityIndicator.isHidden = true
+        generateCodeButton.alpha = 0
+        copyCodeButton.alpha = 0
+        closeButton.alpha = 0
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        print("setSelected \(selected) for \(self.credential!.account)")
+        guard let credential = credential else { return }
+
+        if selected {
+            UIView.animate(withDuration: 0.5) {
+                self.blurView.effect = UIBlurEffect(style: .light)
+                if credential.code.isEmpty || credential.type == .HOTP {
+                    self.generateCodeButton.alpha = 1.0
+                    self.generateCodeButton.isEnabled = true
+                } else {
+                    self.generateCodeButton.alpha = 0.3
+                    self.generateCodeButton.isEnabled = false
+                }
+                if !credential.code.isEmpty {
+                    self.copyCodeButton.alpha = 1
+                    self.copyCodeButton.isEnabled = true
+                } else {
+                    self.copyCodeButton.alpha = 0.3
+                    self.copyCodeButton.isEnabled = false
+                }
+                
+
+                self.closeButton.alpha = 1
+            }
+        } else {
+            UIView.animate(withDuration: 0.3) {
+                self.blurView.effect = nil
+                self.generateCodeButton.alpha = 0
+                self.copyCodeButton.alpha = 0
+                self.closeButton.alpha = 0
+            }
+        }
     }
     
     // this method is invoked when table view reloaded and UI got data/list of credentials
@@ -142,6 +209,13 @@ class CredentialTableViewCell: UITableViewCell {
         }
 
         var otp = credential.code.isEmpty ? "******" : credential.code
+        
+        if self.isSelected && credential.code.isEmpty {
+            self.copyCodeButton.isEnabled = false
+            self.copyCodeButton.alpha = 0.3
+            self.generateCodeButton.isEnabled = true
+            self.generateCodeButton.alpha = 1.0
+        }
 
         if credential.isSteam {
             self.code.text = otp
